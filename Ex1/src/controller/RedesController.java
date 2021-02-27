d4bea071412cb73534308abc026c3f91ee1054f9
lusca01 <lucas_dd@live.com>
2021-02-26 23:07:15 -0300

    Ex1

---
+++ Ex1/src/controller/RedesController.java
@@ -0,0 +1,131 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RedesController {
+	
+//O primeiro, chamado ip, que recebe o nome do Sistema Operacional como
+//parâmetro e, de acordo com o S.O., faz a chamada de configuração de IP e
+//filtra a saída do processo, retornando um String com o nome do Adaptador
+//Ethernet e o IPv4 apenas (Não importa o número de adaptadores ethernet,
+//devem aparecer todos). Os adaptadores que não tiverem IPv4 não devem
+//ser mostrados.	
+	public void ip (String so) {
+		if (so.contains("Windows")) {
+			try {
+				Process x = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo = x.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if(linha.contains("Ethernet") || linha.contains("IPv4.")) {
+					System.out.println(linha);
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				Process x = Runtime.getRuntime().exec("ifconfig");
+				InputStream fluxo = x.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if(linha.contains("flags=") || linha.contains("inet ")) {
+					System.out.println(linha);
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+
+//	O segundo, chamado ping, que recebe o nome do Sistema Operacional
+//	como parâmetro e, de acordo com o S.O., faz a chamada de ping com 10
+//	iterações, filtra a saída, pegando apenas o tempo e dá a saída, em ms, do
+//	tempo médio do ping. (O endereço para ping, pode ser o
+//	www.google.com.br) 
+	public void ping (String so) {
+		if (so.contains("Windows")){
+			try {
+				Process x = Runtime.getRuntime().exec("ping -4 -n 10 www.uol.com.br");
+				InputStream fluxo = x.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				int c = 0;
+				while (c <= 11) {
+					String[] tempo = linha.split(" ");
+					for (String palavras : tempo) {
+						if (palavras.contains("tempo")) {
+						System.out.println(palavras);	
+						}
+					}
+					linha = buffer.readLine();
+					c++;
+				}
+				while (linha != null) {
+					String[] tempo = linha.split(",");
+					for (String palavras : tempo) {
+						if (palavras.contains("dia")) {//Coloquei "dia" porque "Média" dá erro no "é"
+						System.out.println(palavras);	
+						}
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			try {
+				Process x = Runtime.getRuntime().exec("ping -4 -c 10 www.uol.com.br");
+				InputStream fluxo = x.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				int c = 0;
+				while (c <= 11) {
+					String[] tempo = linha.split(" ");
+					for (String palavras : tempo) {
+						if (palavras.contains("tempo")) {
+						System.out.println(palavras +"ms");	
+						}
+					}
+					linha = buffer.readLine();
+				}
+				while (linha != null) {
+
+					if (linha.contains("min/avg")) {
+						System.out.println(linha);	
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
---
+++ Ex1/src/view/Main.java
@@ -0,0 +1,51 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.RedesController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RedesController controller = new RedesController();
+		int op = 0;
+		op = Integer.parseInt(JOptionPane.showInputDialog("1 - IP do Host \n2 - PING do Uol \n3 - EXIT"));
+		String os = System.getProperty("os.name");
+		switch (op) {
+		case 1:
+			controller.ip(os);
+			break;
+		case 2:
+			controller.ping(os);
+			break;
+		case 3:
+			JOptionPane.showMessageDialog(null,"Encerrando...");
+			break;
+		default:
+			JOptionPane.showMessageDialog(null,"Opção inválida.");
+		}
+	}
+}
+
+//Para auxiliar na resolução dos exercícios:
+//para verificar informações de adaptadores ethernet:
+//Windows: ipconfig
+//Linux: ifconfig
+//
+//para definir quantidade de pings (-n ou -c é o número de iterações, -4 é para forçar a operação em IPv4):
+//Exemplo Windows: ping -4 -n 10 www.uol.com.br
+//Exemplo Linux: ping -4 -c 10 www.uol.com.br
+//
+//para listar os processos:
+//Windows: TASKLIST /FO TABLE
+//Linux: ps -ef
+//star 1 like 1
+//para matar processo por PID:
+//Windows: TASKKILL /PID numero_do_PID
+//Linux: kill -9 numero_do_PID
+//like 1
+//para matar processo por Nome:
+//Windows: TASKKILL /IM nome_do_processo
+//Linux: pkill -f nome_do_processo
+//like 1
+
